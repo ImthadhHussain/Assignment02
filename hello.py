5cb5abe7caafba693660461e3c31c89148b6a5e1
unknown <imthadhusain2000@gmail.com>
2024-12-21 18:34:01 +0530

    Python Code

---
+++ hello.py
@@ -0,0 +1,5 @@
+name = "Imthadh Hussain"
+sID = "S24013333"
+uniName = "Open University of Sri Lanka"
+
+print("Hello! My Name is "+ name + ". I am studying BSc in IT " + uniName +"and this is my student id number " + sID)
